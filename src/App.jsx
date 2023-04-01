80f659835e5e72adb38cd1a2ccf40a4cc27e187f
Ernest Anyewe Adonu <ernestadonu@yahoo.com>
2023-04-01 00:28:34 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/Login';
+import Error from './pages/Error';
 
 function App() {
   return (
     <>
-      <h1>React App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </>
   );
 }
