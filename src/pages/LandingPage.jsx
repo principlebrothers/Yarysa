9747545d2bb2653d280228aba744e86490f713d6
Ernest Anyewe Adonu <ernestadonu@yahoo.com>
2023-04-01 02:11:07 +0000

    Create LandingPage.jsx

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from '../components/container/Container';
+import Footer from '../components/footer/Footer';
+import Navbar from '../components/header/Navbar';
+import Main from '../components/main/Main';
+
+function LandingPage() {
+  return (
+    <Container>
+      <Navbar />
+      <Main />
+      <Footer />
+    </Container>
+  );
+}
+
+export default LandingPage;
