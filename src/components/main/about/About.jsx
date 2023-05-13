42818b5c96748065012423bde64b1e7b1c2011e9
Ernest Anyewe Adonu <ernestadonu@yahoo.com>
2023-05-13 16:17:57 +0000

    Update information in the about section

--- src/components/main/about/About.jsx
+++ src/components/main/about/About.jsx
@@ -13,34 +13,30 @@ function About() {
       <div className={styles.top__content}>
         <div className={styles.section__logo}>
           <p>
-            <HiOutlineLightBulb className="updaged__secondary__color medium__font" />
+            <HiOutlineLightBulb className='updaged__secondary__color medium__font' />
           </p>
         </div>
         <div className={styles.section__title}>
-          <span className="updaged__secondary__color">
+          <span className='updaged__secondary__color'>
             MEDICAL BILLING DEVELOPMENT PLATFORM
           </span>
-          <h2 className="general__font__color">
+          <h2 className='general__font__color'>
             The complete Toolkit for patient records and healthcare
             prescriptions
           </h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur. Nullam morbi et tellus
-            malesuada sed maecenas. Nisl cursus ipsum pretium ultrices at.
-            Ullamcorper nibh ridiculus vestibulum aliquet. Suscipit nisl purus
-            nunc elementum ultricies. Duis tristique et amet convallis
-            fermentum. A sed amet orci vulputate sed blandit dolor pharetra.
+            Experience the Future of Healthcare with Yarysa EMR's Complete
+            Toolkit for Patient Records and Prescriptions - Transform Your
+            Practice with Cutting-Edge Technology and Streamlined Workflows.
           </p>
-          <span className="flex align__center gap updaged__secondary__color small__font">
-            Check our pricing
-            {' '}
-            <MdArrowForwardIos className="arrow" />
+          <span className='flex align__center gap updaged__secondary__color small__font'>
+            Check our pricing <MdArrowForwardIos className='arrow' />
           </span>
         </div>
       </div>
       <div className={styles.middle__content}>
         <div className={styles.left_content}>
-          <img src={nurses} alt="portfolio" height="300px" width="416px" />
+          <img src={nurses} alt='portfolio' height='300px' width='416px' />
         </div>
       </div>
       <div className={styles.buttom__content}>
@@ -56,16 +52,14 @@ function About() {
         </div>
       </div>
       <div className={styles.final__container}>
-        <p className="primary__color padding">
+        <p className='primary__color padding'>
           Start protecting patients and improve healthcare today
         </p>
         <button
-          type="button"
+          type='button'
           className={`${styles.btn} flex gap justify__center `}
         >
-          GET STARTED
-          {' '}
-          <IoIosArrowRoundForward className="arrow" />
+          GET STARTED <IoIosArrowRoundForward className='arrow' />
         </button>
       </div>
     </section>
