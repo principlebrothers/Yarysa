fd123ffcb0e058b0b666a08413dfafb8ae5398f9
Ernest Anyewe Adonu <ernestadonu@yahoo.com>
2023-05-13 16:21:23 +0000

    Update information in the portfolio section

--- src/components/main/porfolio/Portfolio.jsx
+++ src/components/main/porfolio/Portfolio.jsx
@@ -13,12 +13,9 @@ function Portfolio() {
           <p>{'</>'}</p>
         </div>
         <div className={styles.section__title}>
-          <span className="updaged__secondary__color">
-            BUILT FOR HOSPITALS
-          </span>
-          <h2 className="general__font__color">
-            Build magical automations for medical billings with Yarysa
-            {' '}
+          <span className='updaged__secondary__color'>BUILT FOR HOSPITALS</span>
+          <h2 className='general__font__color'>
+            Build magical automations for medical billings with Yarysa{' '}
           </h2>
           <p>
             Lorem ipsum dolor sit amet consectetur. Nullam morbi et tellus
@@ -27,26 +24,24 @@ function Portfolio() {
             nunc elementum ultricies. Duis tristique et amet convallis
             fermentum. A sed amet orci vulputate sed blandit dolor pharetra.
           </p>
-          <span className="flex align__center gap updaged__secondary__color small__font">
-            Check our pricing
-            {' '}
-            <MdArrowForwardIos className="arrow" />
+          <span className='flex align__center gap updaged__secondary__color small__font pointer'>
+            Check our pricing <MdArrowForwardIos className='arrow' />
           </span>
         </div>
       </div>
       <div className={styles.buttom__content}>
         <div className={styles.left_content}>
-          <img src={checklist} alt="portfolio" width="600px" />
+          <img src={checklist} alt='portfolio' width='600px' />
         </div>
         <div className={styles.right_content}>
           <div className={styles.portfolio__data}>
             {portfolioData.map((data) => (
               <div className={styles.portfolio__data__items} key={data.id}>
-                <h3 className="updaged__secondary__color">{data.title}</h3>
+                <h3 className='updaged__secondary__color'>{data.title}</h3>
                 <div className={styles.data__item}>
                   <p>{data.details}</p>
-                  <span className="padding">
-                    <data.icon className="updaged__secondary__color " />
+                  <span className='padding'>
+                    <data.icon className='updaged__secondary__color ' />
                   </span>
                 </div>
               </div>
