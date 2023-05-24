bc982ecd8015c5b0d5c7b65533385b8db8b8ea3e
principlebrothers <principlesbrothers@yahoo.com>
2023-05-24 15:09:35 +0000

    Update navigation bar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -4,6 +4,7 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import yarysaLogo from '../../assets/yarysaLogo.svg';
 import { navLinks } from '../utils/Utils';
 
+import 'animate.css';
 import styles from './Navbar.module.css';
 
 function Navbar() {
@@ -16,33 +17,46 @@ function Navbar() {
   return (
     <nav>
       <div className={styles.mobile__menu}>
-        <div className={styles.logo__container}>
-          <img src={yarysaLogo} alt="logo" width="225" height="33" />
-        </div>
+        <figure className={styles.logo__container}>
+          <img src={yarysaLogo} alt='logo' width='225' height='33' />
+        </figure>
         <AiOutlineMenu
           className={`${styles.nav__menu}`}
           onClick={handleLinksDisplay}
         />
       </div>
       <section
-        id="nav"
+        id='nav'
         className={`${styles.nav} ${isActive ? styles.active : ''}`}
       >
-        <div className={styles.desktop__logo__container}>
-          <img src={yarysaLogo} alt="logo" width="225" height="33" />
-        </div>
+        <figure
+          className={`${styles.desktop__logo__container} animate__animated animate__shakeY`}
+        >
+          <img src={yarysaLogo} alt='logo' width='225' height='33' />
+        </figure>
         <div className={`${styles.nav__links}`}>
-          {navLinks.map((link) => (
-            <Link to={link.path} key={link.id} className={styles.nav__font}>
-              {link.page}
-            </Link>
-          ))}
+          {navLinks.map((link) =>
+            link.id === 'about' ? (
+              <a
+                href={link.path}
+                key={link.id}
+                className={styles.nav__font}
+                target='_blank'
+              >
+                {link.page}
+              </a>
+            ) : (
+              <a href={link.path} key={link.id} className={styles.nav__font}>
+                {link.page}
+              </a>
+            )
+          )}
         </div>
         <div className={`${styles.nav__action__container} `}>
-          <Link to="/login" className={`${styles.nav__font}`}>
+          <Link to='/login' className={`${styles.nav__font}`}>
             LOG IN
           </Link>
-          <button type="button">GET STARTED</button>
+          <button type='button'>REQUEST DEMO</button>
         </div>
       </section>
     </nav>
