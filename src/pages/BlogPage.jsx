91e6b8898f76dd7f7fdb232ff106deb9f4c8ccb2
principlebrothers <principlesbrothers@yahoo.com>
2023-06-16 22:46:43 +0000

    Create blog page

---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Container from '../components/container/Container';
+import Footer from '../components/footer/Footer';
+import Blogs from '../components/blogs/Blogs';
+import Navbar from '../components/blogs/header/Navbar';
+
+
+function BlogPage() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        backgroundColor: '#2f2e41',
+      }}
+    >
+      <Navbar />
+      <div className='responsive-container'>
+        <Blogs />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default BlogPage
