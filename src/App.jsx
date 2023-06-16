058e063ba4270dba9cfed9ac34e15a4ce0315fbd
principlebrothers <principlesbrothers@yahoo.com>
2023-06-16 22:45:37 +0000

    Add route to blogs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/Login';
 import Error from './pages/Error';
+// import Blogs from './components/blogs/Blogs';
+import BlogPage from './pages/BlogPage';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/blogs" element={<BlogPage />}/>
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
